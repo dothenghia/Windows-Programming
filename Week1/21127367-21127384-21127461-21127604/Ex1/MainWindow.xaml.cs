82e88734f6d0f81d77497fb327f190ad0f4e6bc8
Thế Nghĩa <nghiask2018@gmail.com>
2024-01-18 15:14:09 +0700

    Week 2

---
+++ Week1/21127367-21127384-21127461-21127604/Ex1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace Ex1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Kiểm tra thông tin đăng nhập
+            if (username == "admin" && password == "qwe3@1")
+            {
+                // Chuyển hướng đến Dashboard
+                DashboardWindow dashboard = new DashboardWindow();
+                dashboard.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password");
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Sử dụng Alt + U để chuyển đến ô văn bản Username
+            if (e.SystemKey == Key.U && (Keyboard.Modifiers & ModifierKeys.Alt) == ModifierKeys.Alt)
+            {
+                txtUsername.Focus();
+            }
+
+            // Sử dụng Alt + P để chuyển đến ô văn bản Password
+            if (e.SystemKey == Key.P && (Keyboard.Modifiers & ModifierKeys.Alt) == ModifierKeys.Alt)
+            {
+                txtPassword.Focus();
+            }
+        }
+    }
+}
+
---
+++ Week1/Ex1/DashboardWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Ex1
+{
+    public partial class DashboardWindow : Window
+    {
+        private string[] imagePaths = { "images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg", "images/6.jpg", "images/7.jpg" };
+        private string[] quotes = { "There is nothing impossible to they who will try.", "Champions keep playing until they get it right.", "With great power comes great responsibility", "You are your best thing.", "A person who never made a mistake never tried anything new.", "Believe you can and you're halfway there.", "Love yourself first and everything else falls into line.", "Be yourself; everyone else is already taken." };
+        private int currentImageIndex = 0;
+
+        public DashboardWindow()
+        {
+            InitializeComponent();
+            ShowImage();
+        }
+
+        private void ShowImage()
+        {
+            if (currentImageIndex >= 0 && currentImageIndex < imagePaths.Length)
+            {
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imagePaths[currentImageIndex], UriKind.Relative));
+                imgSlideshow.Source = bitmapImage;
+                txtCaption.Text = quotes[currentImageIndex];
+            }
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            currentImageIndex = (currentImageIndex - 1 + imagePaths.Length) % imagePaths.Length;
+            ShowImage();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            currentImageIndex = (currentImageIndex + 1) % imagePaths.Length;
+            ShowImage();
+        }
+    }
+}
