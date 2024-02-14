9d5e20da43eb2f7de411bc6dc4469d59acbd1396
Thế Nghĩa <nghiask2018@gmail.com>
2024-02-14 23:30:06 +0700

    Complete UI

--- Ass1/Ass01_21127367/MainWindow.xaml.cs
+++ Ass1/Ass01_21127367/MainWindow.xaml.cs
@@ -8,12 +8,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics; // Để sử dụng Debug.WriteLine
+using System.IO;
 
 namespace Ass01_21127367
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -21,6 +20,37 @@ namespace Ass01_21127367
             InitializeComponent();
         }
 
+        // Responsive size for Grid Column of LEFT & RIGHT WINDOW
+        private void LeftListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (LeftList_ListView.View != null && LeftList_ListView.View is GridView grid)
+            {
+                grid.Columns[0].Width = LeftWindow_DockPanel.ActualWidth / 4;
+                grid.Columns[1].Width = LeftWindow_DockPanel.ActualWidth / 4;
+                grid.Columns[2].Width = LeftWindow_DockPanel.ActualWidth / 4;
+                grid.Columns[3].Width = LeftWindow_DockPanel.ActualWidth / 4;
+            }
+
+            if (ToolBar_Border != null)
+            {
+                ToolBar_Border.Width = LeftWindow_DockPanel.ActualWidth + RightWindow_DockPanel.ActualWidth;
+            }
+        }
+        private void RightListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (RightList_ListView.View != null && RightList_ListView.View is GridView grid)
+            {
+                grid.Columns[0].Width = RightWindow_DockPanel.ActualWidth / 4;
+                grid.Columns[1].Width = RightWindow_DockPanel.ActualWidth / 4;
+                grid.Columns[2].Width = RightWindow_DockPanel.ActualWidth / 4;
+                grid.Columns[3].Width = RightWindow_DockPanel.ActualWidth / 4;
+            }
+
+            if (ToolBar_Border != null)
+            {
+                ToolBar_Border.Width = LeftWindow_DockPanel.ActualWidth + RightWindow_DockPanel.ActualWidth;
+            }
+        }
 
     }
 }
--- Ass1/Ass01_21127367/obj/Debug/net8.0-windows/Ass01_21127367.AssemblyInfo.cs
+++ Ass1/Ass01_21127367/obj/Debug/net8.0-windows/Ass01_21127367.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ass01_21127367")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e9148b492d32dcecc50f15be3966afc75cfc2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f526b95e056eb70c15b8e54abf2b58d7ebfc00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ass01_21127367")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ass01_21127367")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
